1feb70e2b6ddf5727986a6c46ac61b1073cd81f8
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-12 16:52:26 -0800

    5 and 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * starts at main 
+ * uses printf command and
+ * returns zero*/
+int main(){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * this program prints the size of the different data types for each*/
+int main(){
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a float %lu byte(s)\n", sizeof(float));
+	return (0);
+}
