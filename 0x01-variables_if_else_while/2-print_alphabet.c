576833b08a7d51a4cd9bce7745e69759e2831dbc
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-14 05:47:45 -0700

    question 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,12 @@
 int main(void)
 {
 	char alphabet = 'a';
-	while(alphabet != 'z')
+
+	while (alphabet <= 'z')
 	{
 		putchar(alphabet);
 		alphabet++;
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
