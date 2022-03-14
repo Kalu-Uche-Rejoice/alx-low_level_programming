1f5ed859eab07a94629f47581b7bb5d2ef439317
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-14 06:21:51 -0700

    question 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the program starts here
+ * Description: The program prints all numbers in base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	return (0);
+}
