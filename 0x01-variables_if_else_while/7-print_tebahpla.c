b662d4f407146c49b4b72730cd885f48202785a6
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-14 09:46:20 -0700

    question 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this is where the program starts
+ * Description: The program prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet = 'z';
+
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
