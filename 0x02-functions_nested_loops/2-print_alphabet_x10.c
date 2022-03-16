ce846e022fb2d68ecd9636c2959bff0dbcaab87b
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-15 19:27:02 -0700

    solution

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char alpha = 'a';
-	int n = 0;
+	char alpha;
+	int n;
 
-	while (n < 10)
+	for (n = 0; n < 10; n++)
 	{
-		while (alpha <= 'z')
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
-			alpha++;
 		}
 		_putchar('\n');
-		n++;
 	}
 }
