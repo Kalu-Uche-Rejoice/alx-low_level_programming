e348ef4c12431e91154ab73d96ece8bdec96d948
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-16 01:11:29 -0700

    .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,23 +7,29 @@
  */
 void times_table(void)
 {
-	for (int i = 0; i <= 9; i++)
+	int i = 0;
+	while (i <= 9)
 	{
-		for (int j = 0; j <= 9; j++)
+		int j = 0;
+		while (j <= 9)
 		{
-			if ( (i * j) > 9)
+			int p = i * j;
+
+			if ( p > 9)
 			{
-				_putchar('0' + ((i * j) / 10);
-				_putchar('0' + ((i * j) % 10);
+				_putchar('0' + (p / 10);
+				_putchar('0' + (p % 10);
 				_putchar(',');
 				_putchar(' ');
 			}
 			else
 			{
-				_putchar('0' + (i * j));
+				_putchar('0' + p);
 				_putchar(',');
 				_putchar(' ');
 			}
+			j++;
 		}
+		i++;
 	}
 }
