d88016afa647f62e49f06d87a69e5de0de8386a9
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-15 18:51:06 -0700

    solution

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+/**
+ * _putchar - writes character to stdout
+ * Description: one character at a time
+ * Return: on success 0
+ */
+int _putchar(char c);
+
+/**
+ * print_alphabet - print alphabet in lower case
+ * Description: uses _putchar
+ * Return: void
+ */
+void print_alphabet(void);
+
