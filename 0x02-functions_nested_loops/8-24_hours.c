d918d45aa09bfbea84c7d3a374b4a59bcd85219b
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-15 20:57:47 -0700

    solution

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - computes every minute of the day
+ * Description: uses nested loops
+ * Retun: void
+ */
+void jack_bauer(void)
+{
+	int m;
+	int h
+		;
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
+		}
+	}
+}
+
