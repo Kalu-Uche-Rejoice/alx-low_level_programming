17752576bdf7c73b117b98d6c3fd00a04b622f78
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-17 05:51:00 -0700

    .

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/** _isdigit - checks if a value is a digit
+ * Description: usef for loops and conditionals
+ * @c: value to be checked
+ * Return: 1 if it is a digit and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int i = 0;
+	int j;
+
+	if (c < 10 && c >= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+int main (void)
+{
+	int _isupper(int c);
+	int _isdigit(int c);
+
+	return (0);
+}
