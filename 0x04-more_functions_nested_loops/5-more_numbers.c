feffa75cf8ea12f5baa711f6711b084a3099d898
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-18 06:46:53 -0700

    second chance

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -18,12 +18,8 @@ void more_numbers(void)
 			if (j > 9)
 			{
 				_putchar('0' + (j / 10));
-				_putchar('0' + (j % 10));
 			}
-			else
-			{
-				_putchar('0' + j);
-			}
+			_putchar('0' + (j % 10));
 			j++;
 		}
 		_putchar('\n');
