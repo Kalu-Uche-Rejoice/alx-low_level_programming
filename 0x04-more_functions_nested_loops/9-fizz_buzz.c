11146e5bab5b30796d775f4dcde452e0902a13d4
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-17 20:05:28 -0700

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints to screen fizz for multiples of 3,
+ *  buzz for multiples of 5, fizz buzz for multiples
+ *  of 5 and 3, otherwise it prints number. range: 1-100
+ *  Description: uses while loop and conditional if statements
+ *  Return: 0
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if ((i % 3) != 0 && (i % 5) != 0)
+		{
+			printf("%d",i);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("fizz");
+		}
+		if (i % 5 == 0)
+		{
+			printf("buzz");
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
