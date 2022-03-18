27a825af6b6fcc2b06bc851590bb2a48a4b1b5de
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-17 20:28:07 -0700

    8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - prints equal row by columns of #
+ * Description: uses nested for loops
+ * @size: tells number of rows or columns to print
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i = 0;
+
+	while (i < size)
+	{
+		int j = 0;
+
+		while (j < size)
+		{
+			_putchar('#');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
