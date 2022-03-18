fd3781d95d8ea68aeb1144e6d4581b8212800569
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-17 18:36:10 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,12 @@
-int main (void)
-{
-	int _isupper(int c);
-	int _isdigit(int c);
+#ifndef main_h
+#define main_h
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
 
-	return (0);
-}
+#endif
+
