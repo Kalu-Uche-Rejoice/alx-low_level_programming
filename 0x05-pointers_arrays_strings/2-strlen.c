16d0df71ec5120ea54a447a0c96d6ddebec4ea3d
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-21 14:57:58 -0700

    two

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - computes the length of a string
+ * Description: uses while loops to count the number of characters till 
+ * string escape '\0'
+ * @*s: string whose characters are to be counted
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length += 1;
+		s++;
+	}
+	return (length);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,3 +7,6 @@ void reset_to_98(int *n);
  * swap_int - (swaps the values of two integers)
  */
 void swp_int(int *a, int *b);
+/** _strlen - returns the length of a string
+ */
+int _strlen(char *s);
