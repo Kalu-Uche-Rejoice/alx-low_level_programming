7a6805299e09e3509d72f012bf32436eb954ebad
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-24 23:35:10 -0700

    two

---
+++ 0x06-pointers_arrays_strings/2-strncp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - function copies a string
+ * Description: simmilar to strncpy built C function, uses for loops
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: number of characters to be copied
+ * Return: destination pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return *dest;
+}
