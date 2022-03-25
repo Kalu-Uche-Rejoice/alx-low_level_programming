f5c2a977d633a99faf10d1e0b4d8df103f615a0c
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-24 23:46:27 -0700

    two

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main.h";
 /**
  * _strncpy - function copies a string
  * Description: simmilar to strncpy built C function, uses for loops
@@ -15,5 +15,5 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 	}
-	return *dest;
+	return dest;
 }
