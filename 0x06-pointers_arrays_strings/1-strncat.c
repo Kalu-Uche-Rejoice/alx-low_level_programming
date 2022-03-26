fdeaca7e76d31d3915ff1a89c9ccc3a508557e0b
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-25 17:18:49 -0700

    one

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strncat - appends a portion of src
+ * string to dest string
+ * Description: uses while loops to determine string
+ * length, uses for loop
+ * @dest: destination
+ * @src: source string
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0;
+	int i = 0;
+	
+	while (count >= 0)
+	{
+		if (dest[count] == '\0')
+		{
+			break;
+		}
+		count++;
+	}
+	for (; i < n && src[i] != '\0'; i++)
+	{
+		if(dest[count + i] == '\0')
+		{
+			dest[count + i] = src[i];
+		}
+	}
+	return (dest);
+}
