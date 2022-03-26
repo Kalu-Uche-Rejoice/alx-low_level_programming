ec5290e9bea7d31e7fa215d5cc06d964965bd091
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-25 19:58:18 -0700

    five

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * string_toupper - converts lowercase alphabets to upper
+ * case
+ * Description: uses conditionals and a while loop
+ * @a: string for conversion
+ * Return: char a
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+	
+	while (i >= 0)
+	{
+		if ( a[i] <= 122 && a[i] >= 97)
+		{
+			a[i] = a[i] - 32;
+		}
+		if (a[i] == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+}
