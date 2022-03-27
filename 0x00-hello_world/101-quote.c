46022955c0b2227576101f95eb5160b7c02420c0
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-27 08:47:19 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "\n");
 	return (1);
 }
