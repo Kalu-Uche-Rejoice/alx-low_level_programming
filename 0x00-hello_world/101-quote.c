1b7335de10918189fb9aa585b71ddd6cf214d774
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-27 08:34:00 -0700

    correction

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**main - entry point
+ * Description: outputs specific string
+ * Return: 1
+ */
+int main (void)
+{
+	fprintf(stderr, "and that peice of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
