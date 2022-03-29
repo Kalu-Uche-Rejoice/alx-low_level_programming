c7c6d5b335411d6d47ea1d896e8dbff92a921775
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-28 18:26:31 -0700

    two

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * Description: uses coinditionals and a while loop
+ * @s: string to be checked
+ * @c: character to be found
+ * Return: q
+ */
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+	char *q;
+
+	while ( count >= 0)
+	{
+		if (*(s + count) == c)
+		{
+			q = (s + count);
+			break;
+		}
+		else if (*(s + count) == '\0')
+		{
+			q = NULL;
+			break;
+		}
+		count++;
+	}
+	return (q);
+}
