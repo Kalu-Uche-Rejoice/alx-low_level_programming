878a1e8b50643c6f528ecb193cdcc3fa21caa9d5
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-03-28 17:02:55 -0700

    zero

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * memset - fills memory with a constant byte
+ * Description: uses a while loop
+ * @s: pointer to address to be filled
+ * @b: pointer with address containing filler
+ * @n: number of bytes to be filled
+ * Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int count = 0;
+
+	while(count < n)
+	{
+		s[count] = b;
+		count++;
+	}
+	return (s);
+}
