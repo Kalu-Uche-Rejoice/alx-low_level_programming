ea814ab694cd79e821b6b0c397c97949a8785f9e
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-04-03 16:03:04 -0700

    correction

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,3 +5,24 @@
  * @a: string
  * Return: a
  */
+char *leet(char *a)
+{
+	int ascii[5] = {4, 3, 7, 0, 1};
+	int cap[5] = {65, 69, 79, 84, 76};
+	int small[5] = {97, 101, 111, 116, 108}
+	int i = 0, j;
+	
+	while (*(a + i) != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (*(a + i) == cap[j] || *(a + i) == small[j])
+			{
+				*(a + i) = ascii[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (a);
+}
