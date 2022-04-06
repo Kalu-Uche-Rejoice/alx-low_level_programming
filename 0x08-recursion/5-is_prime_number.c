57aff6832f7771649868d2207c35fa813dba1866
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-04-06 05:30:51 -0700

    correction

---
+++ 0x08-recursion/5-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * is_prime_number - calls recursive function
+ * @n: integer to be tested
+ * Return: 1 for prime number 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
+}
+
+/**
+ * prime - checks if a number is a prime number
+ * @n: number to be checked
+ * @g: counter
+ * Return: 1 for prime number 0 otherwise
+ */
+int prime(int n, int g)
+{
+	if (n % g == 0)
+	{
+		if (n == g)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return(prime(n, g + 1));
+}
