68fc12a15bd66a4d68ff6b2f570f4fff28af60ec
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-04-07 05:30:11 -0700

    correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: array of args
  * Return: zero
  */
-int main(int argc, __attribute__((unused))char *argv[])
+int main(int argc__attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return 0;
