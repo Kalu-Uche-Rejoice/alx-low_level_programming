3b6d9d5f5c33e0c56a5c113f9daa72488aa2bd8d
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-04-11 08:51:31 -0700

    0x0E-structures

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is my attempt on  the alx project on structures in c.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - initialize variable
+ * @name: name of dog
+ * @age: type float
+ * @owner: type string
+ *
+ * Description: A structure describing a dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
