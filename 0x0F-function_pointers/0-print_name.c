81d4e04f7074aadff26d548e13658ff17df6a26f
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-04-13 06:40:12 -0700

    correction one

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "functions_pointers.h"
+#include "function_pointers.h"
 /**
  * print_name - functio to print a name
  * @name: pointer to name
  * @f: pointer to function
  * Return: void
  */
-void print_name(char *name, void (*f)(char))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
 	{
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include<stdio.h>
+#include <stddef.h>
 #include "function_pointers.h"
 /**
  * array_iterator - function that executes a
