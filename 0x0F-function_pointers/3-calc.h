4bed617ee909dd8fb6d2a958ebdc5d59dbc0bb93
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-04-14 04:18:17 -0700

    correcton

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,5 @@
+#ifndef CALC
+#define CALC
 /**
  * struct op - structure of operator
  * @op: operator
@@ -7,10 +9,11 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}op_t;
+} op_t;
 int (*get_op_func(char *s))(int, int);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+#endif /*ends the definition of the header acts as a file guard*/
