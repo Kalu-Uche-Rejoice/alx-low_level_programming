9aff45d640426a528774316cfe9db84c2c799395
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-04-19 05:23:05 -0700

    correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 	}
 	while (i < argc)
 	{
-		if ((**(argv + i) >= 0  && **(argv + i) <= 9))
+		if (!(**(argv + i) >= 0  && **(argv + i) <= 9))
 		{
 			sum += atoi(*(argv + i));
 		}
