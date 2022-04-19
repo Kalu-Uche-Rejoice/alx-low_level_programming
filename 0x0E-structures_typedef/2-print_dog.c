fdbeb445840a69d6c272548af896fad5e5e60f74
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-04-19 06:10:42 -0700

    correction

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,15 +7,15 @@
  */
 void print_dog(struct dog *d)
 {
-	if(d == NULL)
+	if (d == NULL)
 	{
 		return;
 	}
 	(d->name == NULL) ? printf("Name: (nil)\n")
-		:printf("Name: %s\n", d->name);
-	(d->age <= 0) ?printf("Age: (nil)\n")
-		:printf("Age: %f\n", d->age);
+		: printf("Name: %s\n", d->name);
+	(d->age < 0) ? printf("Age: (nil)\n")
+		: printf("Age: %f\n", d->age);
 	(d->owner == NULL) ? printf("Owner: (nil)\n")
-		:printf("Owner: %s\n", d->owner);
+		: printf("Owner: %s\n", d->owner);
 }
 	
