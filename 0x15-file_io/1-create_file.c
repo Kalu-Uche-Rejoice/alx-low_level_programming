2e5ae08830bc5077f97e1a7ed656a493a7e7abfc
Kalu-Uche-Rejoice <rejoicekaluuche@gmail.com>
2022-05-05 07:29:43 -0700

    attempt

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	/*INSTRUCTION TO OPEN OR CREATE FILE*/
-	fd = open(filename, O_CREAT, 0600);
+	fd = open(filename, O_CREAT|O_WRONLY|O_TRUNC, 0600);
 	/*test for successful open*/
 	if (fd == -1)
 		return (-1);
